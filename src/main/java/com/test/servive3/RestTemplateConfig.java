4346515fff3465a39e7515181f1dfc818b459398
ashanperera <ashanp@gmail.com>
2024-02-22 19:24:58 +0400

    first commit

---
+++ src/main/java/com/test/servive3/RestTemplateConfig.java
@@ -0,0 +1,19 @@
+package com.test.servive3;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+	
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+    
+    
+
+}
---
+++ src/main/java/com/test/servive3/Service3Controller.java
@@ -0,0 +1,20 @@
+package com.test.servive3;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class Service3Controller {
+	
+    @Autowired
+    private RestTemplate restTemplate;
+    
+    @GetMapping("/callService1")
+    public String callService1() {
+        String service1Url = "http://service1/hello";
+        return restTemplate.getForObject(service1Url, String.class);
+    }
+
+}
