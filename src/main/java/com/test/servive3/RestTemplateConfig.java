f60dfa1c05ec94ac4922869269fc6c93da047dc8
Ashan Malinda Perera <ashanp@gmail.com>
2025-01-08 11:40:00 +0400

    Update RestTemplateConfig.java

--- src/main/java/com/test/servive3/RestTemplateConfig.java
+++ src/main/java/com/test/servive3/RestTemplateConfig.java
@@ -1,6 +1,6 @@
 package com.test.servive3;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
