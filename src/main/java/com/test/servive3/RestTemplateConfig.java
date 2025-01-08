ceec438a43ebd3a780e192934a2c65ca2741d52a
Ashan Malinda Perera <ashanp@gmail.com>
2025-01-08 11:37:45 +0400

    Update RestTemplateConfig.java
    remove load balancing because i remove the eureka client from the pom

--- src/main/java/com/test/servive3/RestTemplateConfig.java
+++ src/main/java/com/test/servive3/RestTemplateConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 	
     @Bean
-    @LoadBalanced
+   // @LoadBalanced
     RestTemplate restTemplate() {
         return new RestTemplate();
     }
